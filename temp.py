8cb58065a83748fa905b5b8d2b6997bd0248efb7
natan-sz <nszczepaniak98@gmail.com>
2020-07-17 05:00:33 +0100

    added laplacian

--- temp.py
+++ temp.py
@@ -4,17 +4,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-# Set Resolution of grid
-RES = 100
+# Set Constants
+N = 10
+DA = 1.0
+DB = 0.5
+feed = 0.055
+k = 0.062
+dt = 1
 
-# Create an empty grid
-grid = np.empty((RES, RES))
+# Initialise Chemical A & B
+A = np.ones((N, N))
+B = np.zeros((N, N))
 
-# Fill with random integers
-for i in range(RES):
-    for j in range(RES):
-        grid[i, j] = np.array([1, 2])
+# Now let's add a disturbance in the center
+N2 = N // 2
+radius = r = int(N / 10.0)
 
-# Plot and Show
-plt.imshow(grid, cmap=cm.Greys_r)
-plt.show()
+A[N2 - r : N2 + r, N2 - r : N2 + r] = 0.50
+B[N2 - r : N2 + r, N2 - r : N2 + r] = 0.25
+
+# Find Laplacian
+def laplacian(M, i, j):
+    L = 0
+    L += M[i, j] * -1
+    L += M[i - 1, j] * 0.2
+    L += M[i + 1, j] * 0.2
+    L += M[i, j - 1] * 0.2
+    L += M[i, j + 1] * 0.2
+    L += M[i - 1, j - 1] * 0.05
+    L += M[i + 1, j + 1] * 0.05
+    L += M[i + 1, j - 1] * 0.05
+    L += M[i - 1, j + 1] * 0.05
+    return L
+
+
+# Update A & B
+def update(A, B, DA, DB, feed, k, dt):
+    for i in range(A.ndim):
+        for j in range(A.ndim):
+            diff_A = (
+                (DA * laplacian(A, i, j))
+                - (A[i, j] * B[i, j] ** 2)
+                + (feed * (1 - A[i, j]))
+            ) * dt
+            A[i, j] += diff_A
+
+            diff_B = (
+                (DB * laplacian(B, i, j))
+                + (A[i, j] * B[i, j] ** 2)
+                - ((k + feed) * B[i, j])
+            ) * dt
+            B[i, j] += diff_B
+
+
+while True:
+    update(A, B, DA, DB, feed, k, dt)
+    plt.imshow(A)
+    plt.show()
