c5e973d7e4ef29c530aaea5168625ae9031e34a2
natan-sz <nszczepaniak98@gmail.com>
2020-07-19 13:11:01 +0100

    maths corrent, change animation method

--- temp_0.2.py
+++ temp_0.2.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.animation as animation
-import cv2
 
 # Set Constants
 N = 100
@@ -13,29 +12,35 @@ DB = 0.5
 feed = 0.055
 k = 0.062
 
+# Define Plotting Area
 # fig = plt.figure()
 # ax1 = fig.add_subplot(1, 1, 1)
+
+fig, axes = plt.subplots()
 
 # Initialise Chemicals A & B
 A = np.ones((N, N))
 B = np.zeros((N, N))
 
-# Now let's add a disturbance in the center
+# Add Square to the middle
 N2 = N // 2
-radius = r = int(N / 50.0)
+r = int(N / 10.0)
 
 A[N2 - r : N2 + r, N2 - r : N2 + r] = 0.50
 B[N2 - r : N2 + r, N2 - r : N2 + r] = 0.25
 
-# Initialise Kernel
-def convolution(M):
-    cen = -1
-    adj = 0.2
-    cor = 0.05
+im = plt.imshow(A, animated=True, vmin=0, vmax=1)
 
-    kernel = np.array([[cor, adj, cor], [adj, cen, adj], [cor, adj, cor]])
+cen = -1
+adj = 0.2
+cor = 0.05
 
-    offset = len(kernel) // 2  # Only Compatible with 1:1 matrices
+kernel = np.array([[cor, adj, cor], [adj, cen, adj], [cor, adj, cor]])
+
+offset = len(kernel) // 2  # Only Compatible with 1:1 matrices
+
+# Laplacian Function (TO BE FIXED)
+def convolution(M, kernel, offset):
 
     L = np.zeros((M.shape[0], M.shape[1]))
 
@@ -49,33 +54,21 @@ def convolution(M):
     return L
 
 
-# Update A & B
-def update(A, B, DA, DB, feed, k, N):
+# Update A & B (based on previous iteration)
+def update(A, B, DA, DB, feed, k, N, kernel, offset):
 
-    LA = convolution(A)
-    LB = convolution(B)
-
-    diff_A = ((DA * LA) - (A * B ** 2) + (feed * (1 - A))) * 0.1
-    diff_B = ((DB * LB) + (A * B ** 2) - ((k + feed) * B)) * 0.1
+    diff_A = (DA * convolution(A, kernel, offset)) - (A * B ** 2) + (feed * (1 - A))
+    diff_B = (DB * convolution(B, kernel, offset)) + (A * B ** 2) - ((k + feed) * B)
 
     A += diff_A
     B += diff_B
 
-    return A, B
+
+def animate(i):
+    update(A, B, DA, DB, feed, k, N, kernel, offset)
+    im.set_array(A)
+    return [im]
 
 
-for i in range(100):
-    A, B = update(A, B, DA, DB, feed, k, N)
-
-AS = cv2.resize(A, (800, 800))
-cv2.imshow("reaction", AS)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-# def animate(i):
-#    update(A, B, DA, DB, feed, k, N)
-#    ax1.clear()
-#    ax1.imshow(A)
-#
-#
-# ani = animation.FuncAnimation(fig, animate, interval=100)
-# plt.show()
+ani = animation.FuncAnimation(fig, animate, blit=True, interval=400)
+plt.show()
