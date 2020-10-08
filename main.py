6c82c65325f857c02832de4b3819966d7805e1c0
natan-sz <nszczepaniak98@gmail.com>
2020-10-08 12:44:21 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 # Reaction Diffusion Simulator
+# By Natan Szczepaniak
+#
 # Simulates a reaction between arbitrary chemicals "A" and
 # to "B" to obtain a pattern displayed. The Simulation
-# relies on 5 parameters.
+# relies on 5 parameters. Varying these parameters creates
+# various different patterns.
 #
 # N - resolution of the simulation (aspect ratio 1:1)
-# DA -
+# DA - Diffusion rate of chemical A
+# DB - Diffusion rate of chemical B
+# feed - rate at which the chemical is fed into the system
+# kill - rate at which chemical "dies" off
+#
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,14 +32,13 @@ k = 0.060
 A = np.ones((N, N))
 B = np.zeros((N, N))
 
-# Add Square to the middle
+# Set initial matrix (Add Square to the middle)
 N2 = N // 2
 r = int(N / 10.0)
+A[N2 - r:N2 + r, N2 - r:N2 + r] = 0.50
+B[N2 - r:N2 + r, N2 - r:N2 + r] = 0.25
 
-A[N2 - r : N2 + r, N2 - r : N2 + r] = 0.50
-B[N2 - r : N2 + r, N2 - r : N2 + r] = 0.25
-
-# add scanning kernel
+# Create a scanning kernel
 cen = -1
 adj = 0.2
 cor = 0.05
@@ -40,9 +46,10 @@ cor = 0.05
 kernel = np.array([[cor, adj, cor], [adj, cen, adj], [cor, adj, cor]])
 
 
+# Function updates the matrix by one iteration
 def update(A, B, DA, DB, feed, k, N, kernel):
-    diff_A = (DA * cv2.filter2D(A, -1, kernel)) - (A * B ** 2) + (feed * (1 - A))
-    diff_B = (DB * cv2.filter2D(B, -1, kernel)) + (A * B ** 2) - ((k + feed) * B)
+    diff_A = (DA * cv2.filter2D(A, -1, kernel)) - (A * B**2) + (feed * (1 - A))
+    diff_B = (DB * cv2.filter2D(B, -1, kernel)) + (A * B**2) - ((k + feed) * B)
 
     A += diff_A
     B += diff_B
@@ -59,6 +66,7 @@ fig.subplots_adjust(0, 0, 1, 1)
 fig.tight_layout(pad=0)
 
 
+# Function iterates and animates the matrix evolution
 def animate(i):
     for n in range(40):
         update(A, B, DA, DB, feed, k, N, kernel)
@@ -69,6 +77,7 @@ def animate(i):
 ani = animation.FuncAnimation(fig, animate, blit=True, interval=250)
 plt.show()
 
+## Uncomment to enable non-preview mode (faster)
 ##No live preview (only last frame)
 #iter = 5000
 #
@@ -80,3 +89,9 @@ plt.show()
 #plt.imshow(A)
 #plt.savefig("reaction.png", dpi=300)
 #plt.show()
+
+# TO DO:
+#   - Add documentation
+#   - Create a CLI interface (Setting parameters)
+#   - Check for more FFT methods
+#   - Rewrite in C
